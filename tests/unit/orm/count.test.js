3ba11fd63871b60eb25627fae6d366818fa44b39
Raymundo Salazar <raymundo.salser@hotmail.com>
2020-09-26 10:40:36 -0500

    Validate if exist count on mysql

--- tests/unit/orm/count.test.js
+++ tests/unit/orm/count.test.js
@@ -1,10 +1,10 @@
 import Chunk from '../../setup/models/chunk'
 import Chat from '../../setup/models/chat'
+import Count from '../../setup/models/count'
 
 describe('Model.count()', () => {
 	it('Get total of rows', async function() {
 		let chunks = await Chunk.count();
-		console.log(chunks)
 		expect(typeof chunks).to.be.equal("number")
 		expect(chunks).to.be.greaterThan(117979)
 	})
@@ -13,5 +13,12 @@ describe('Model.count()', () => {
 		let chats = await Chat.where({ messages : "blah" }).count();
 		expect(typeof chats).to.be.equal("number")
 		expect(chats).to.be.greaterThan(0)
+	})
+
+	it('Get zero rows', async function() {
+		let count = await Count.where({ id : 1 }).count();
+		console.log(count);
+		expect(typeof count).to.be.equal("number")
+		expect(count).to.be.equal(0)
 	})
 })
