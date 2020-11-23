6b688dca6f57be72652c355fbfbfef714cd3ef8e
Raymundo Salazar <raymundo.salser@hotmail.com>
2020-11-22 20:18:17 -0600

    validate if prototype exists

--- modules/config/prototypes.config.js
+++ modules/config/prototypes.config.js
@@ -1,22 +1,28 @@
-Object.defineProperty(Array.prototype, 'findByValue', {
-	value: function(key, val) {
-		let resp = false;
-		this.map(obj => {
-			if (obj[key] && obj[key] == val)
-				resp = true;
-		})
-		return resp;
-	}
-});
+if(!Array.prototype.hasOwnProperty("findByValue")){
+	Object.defineProperty(Array.prototype, 'findByValue', {
+		value: function(key, val) {
+			let resp = false;
+			this.map(obj => {
+				if (obj[key] && obj[key] == val)
+					resp = true;
+			})
+			return resp;
+		}
+	});
+}
 
-Object.defineProperty(Object.prototype, 'empty', {
-	get: function() {
-		return Object.entries(this).length === 0
-	}
-});
+if(!Object.prototype.hasOwnProperty("empty")){
+	Object.defineProperty(Object.prototype, 'empty', {
+		get: function() {
+			return Object.entries(this).length === 0
+		}
+	});
+}
 
-Object.defineProperty(Array.prototype, 'wrap', {
-	value: function(key, val) {
-		return this.filter(arr => arr);
-	}
-});
+if(!Array.prototype.hasOwnProperty("wrap")){
+	Object.defineProperty(Array.prototype, 'wrap', {
+		value: function(key, val) {
+			return this.filter(arr => arr);
+		}
+	});
+}
